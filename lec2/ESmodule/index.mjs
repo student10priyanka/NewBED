16fbc7d6ac47b4b458e13098d0096647fe43e3c1
Priyanka Sharma <04.priyanka09.10@gmail.com>
2025-07-04 15:25:58 +0530

    4-7-25

---
+++ lec2/ESmodule/index.mjs
@@ -0,0 +1,11 @@
+export function add(a,b){
+    return a+b;
+}
+export function sub(a,b){
+    return a-b;
+}
+ function mul(a,b){
+    return a*b;
+}
+export default mul;      
+//seafult sef ek hi export ho skta hia eg mul pr agr sare function exort krane hai toh sbse agy export lgana hoga                                                   
